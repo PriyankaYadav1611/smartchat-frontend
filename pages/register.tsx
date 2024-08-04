5585c1356eafd2fe9b593e7cc3b7b45fa1d32271
Priyanka Yadav <priyankayadav8120@gmail.com>
2024-08-04 21:29:19 +0530

    add User Register page

---
+++ pages/register.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/api/users/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Register successful..!");
+      console.log("Got Data: ", data);
+      console.log("Response : ", response);
+    } else {
+      alert('Login failed');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">SignUp</button>
+      </form>
+    </div>
+  );
+}
