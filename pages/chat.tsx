35548284350e033d2665480b791fd261d391afab
Priyanka Yadav <priyankayadav8120@gmail.com>
2024-09-01 15:17:05 +0530

    add extra useEffect having no dependency: it will verify if user isAuthencticated or not,In second useEffect where the dependency is [ isAuthenticated], add Authorization in stompclient: if token is valid then it will connect the stopm otherwise not, add groupId while subscribe stompclient and during send message, message having groupId, with this groupId queue listen for both send and receive message

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useRouter } from 'next/navigation'
 import SockJS from 'sockjs-client';
 import { Stomp } from '@stomp/stompjs';
 import { setSocket } from '../store/websocketSlice';
+import { setToken } from '../store/authSlice';
+import { setUsername } from '../store/meSlice';
 import axios from 'axios';
 
 export default function Chat() {
     const dispatch = useDispatch();
+    const router = useRouter();
     const { token, isAuthenticated } = useSelector((state) => state.auth);
     const { socket, isConnected, stompClient } = useSelector((state) => state.websocket);
     const username = useSelector((state) => state.me.username);
@@ -18,22 +22,42 @@ export default function Chat() {
 
     useEffect(() => {
         console.log("Chat page useeffect called ..................");
-        let me = getMe();
+        // let me = getMe();
+        if (!isAuthenticated) {
+            // User is not authenticated
+            // check if token is present in localStorage
+            const localToken = localStorage.getItem("token");
+            if (!localToken) {
+                // token is not present
+                // send to login page
+                router.push("/login");
+            }
+            // token is present, check token validity
+            getMe();
 
-        if (isAuthenticated || localStorage.getItem("token")) {
-            
+        }
+    }, []);
+
+    useEffect(() => {
+        if (isAuthenticated) {
             const socket = new SockJS('http://localhost:8080/ws');
             
             const stompClient = Stomp.over(() => {
                 return new SockJS('http://localhost:8080/ws')
               });
             
-            stompClient.connect({}, (frame) => {
+              
+            stompClient.connect({Authorization: `Bearer ${localStorage.getItem("token")}`}, (frame) => {
                 console.log("Chat page useeffect websocket connected ..................");
                 dispatch(setSocket({socket: socket, stompClient: stompClient}));
                 console.log("Going to subscribe ...");
                 if (username) {
-                    stompClient.subscribe(`/user/${username}/queue/messages`, (message) => {
+                    // stompClient.subscribe(`/user/${username}/queue/messages`, (message) => {
+                    //     console.log("got msg..", JSON.parse(message.body));
+                    // });
+
+                    stompClient.subscribe(`/user/${username}/queue/messages/groups/26`, (message) => {
+                        // renderMessage(grouId, messageContent, senderId);
                         console.log("got msg..", JSON.parse(message.body));
                     });
                 } else {
@@ -41,12 +65,14 @@ export default function Chat() {
                 }
             });
         }
-    }, []);
+    }, [isAuthenticated])
+
 
     const sendMessage = () => {
         console.log("Chat page inside send message..................");
+        
         if (socket && content.trim() && recipient.trim()) {
-            const message = JSON.stringify({ 'sender': username, 'recipient': recipient, 'content': content });
+            const message = JSON.stringify({ 'senderId': 9, 'groupId': 27, 'content': content });
             stompClient.send("/app/chat.sendMessage", {}, message);
             console.log("Chat page message sent ..................");
             setContent('');
@@ -63,10 +89,14 @@ export default function Chat() {
                     'Authorization': 'Bearer ' + localToken,
                 },
             });
-            console.log("Get me data: ", response);
-            return response;
+            console.log("Get me response: ", response);
+            dispatch(setUsername({username: response.data.username}));
+            dispatch(setToken(localToken))
+            
         } catch (error) {
             console.error("Get me err:", error);
+            localStorage.removeItem("token");
+            router.push("/login");
         }
 
         // let data = '';
